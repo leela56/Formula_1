cd1fba1f6e498c2d10fa5758ce542cacdce50d1c
leelasaikiran4@gmail.com <leelasaikiran4@gmail.com>
2025-01-03 19:37:08 +0000

    7

---
+++ Formula_1 project/includes/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/adlsgen2teama1dev1/raw"
+processed_folder_path = "/mnt/adlsgen2teama1dev1/processed"
+presentation_folder_path = "/mnt/adlsgen2teama1dev1/presentation"
+
+# COMMAND ----------
+
+
