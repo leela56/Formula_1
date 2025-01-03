fc61f8aac9c2f4debda9cd24e28a17861ccd4a81
leelasaikiran4@gmail.com <leelasaikiran4@gmail.com>
2025-01-03 19:36:20 +0000

    4

--- Formula_1 project/ingestion/Circuits_Csv_Ingestion.py
+++ Formula_1 project/ingestion/Circuits_Csv_Ingestion.py
@@ -1,5 +1,18 @@
 # Databricks notebook source
+dbutils.widgets.text("p_data_source", "")
+v_dat_source = dbutils.widgets.get("p_data_source")
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 # MAGIC %run ../includes/configuration
+
+# COMMAND ----------
+
+# MAGIC %run ../includes/Common_functions
 
 # COMMAND ----------
 
@@ -9,7 +22,7 @@
 # COMMAND ----------
 
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp, lit
 
 # COMMAND ----------
 
@@ -52,11 +65,12 @@ circuits_renamed_df = circuits_sel_df.withColumnRenamed('circuitId', 'circuit_id
     .withColumnRenamed('circuitRef', 'circuit_ref') \
     .withColumnRenamed('lat', 'latitude') \
     .withColumnRenamed('lng', 'longitude') \
-    .withColumnRenamed('alt', 'altitude')
+    .withColumnRenamed('alt', 'altitude') \
+    .withColumn("data_source", lit(v_dat_source))
 
 # COMMAND ----------
 
-circuits_final_df = circuits_renamed_df.withColumn('ingestion_date', current_timestamp())
+circuits_final_df = add_ingestion_date(circuits_renamed_df)
 
 # COMMAND ----------
 
@@ -64,4 +78,16 @@ display(circuits_final_df)
 
 # COMMAND ----------
 
-circuits_final_df.write.mode("overwrite").parquet(f'{processed_folder_path}/circuits')
+# MAGIC %sql
+# MAGIC USE CATALOG hive_metastore;
+# MAGIC
+# MAGIC CREATE DATABASE IF NOT EXISTS f1_processed
+# MAGIC MANAGED LOCATION '/mnt/adlsgen2teama1dev1/processed';
+
+# COMMAND ----------
+
+circuits_final_df.write.mode("overwrite").format( "parquet").saveAsTable('f1_processed.circuits')
+
+# COMMAND ----------
+
+
